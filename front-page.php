789b6343e63956ee63eb760ba0d5d3a17b24f824
Vrek <e6236218@cmaisonneuve.qc.ca>
2025-02-11 09:51:27 -0500

    normalement assez proche de ce que le prof a fait au cours que jai manqué (Sem 3 C2)

---
+++ front-page.php
@@ -0,0 +1,55 @@
+<?php get_header() ?>
+<h1>front-page.php</h1>
+    <section class="hero">
+        <div class="hero__contenu global">
+            <h1 class="hero__titre">
+                <?php echo bloginfo('name'); ?>
+            </h1>
+            <p class="hero__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur aspernatur est officiis, mollitia minus asperiores quas libero saepe consequuntur at blanditiis et eligendi, sequi sit quae laboriosam, ex delectus nesciunt.
+            </p>
+            <a href="" class="hero__courriel">
+                info@cmaisonneuve.qc.ca
+            </a>
+            <button class="hero__bouton">
+                Inscription
+            </button>
+            <div class="hero__icone-app">
+                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=paypal&color=000000" width="20" height="20">
+                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
+            </div>
+        </div>
+    </section>
+    <section class="galerie">
+        <figure class="galerie__fig">
+            <img src="" alt="">
+        </figure>
+    </section>
+ 
+    <section class="promotion">
+        <div class="carte carte--grande">
+            <figure class="carte__image">
+                <img src="/4w4/wp-content/themes/TP1/images/img1.jpg" alt="Image de voyage">
+            </figure>
+            <div class="carte__contenu">
+                <h2 class="carte__titre">Destination de rêve</h2>
+                <p class="carte__description">Découvrez des endroits magnifiques à travers le monde.</p>
+                <button class="carte__bouton carte__bouton--actif">Réserver</button>
+            </div>
+        </div>
+    </section>
+    <section class="populaire">
+        <div class="global">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article class="populaire__article">
+                <h2 class="populaire__titre"><?php the_title(); ?></h2>
+                <div class="populaire__contenu"><?php echo wp_trim_words(get_the_content(), 20, "...") ; ?></div>
+            </article>
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
+    <?php get_footer(); ?>
+</body>
+</html>
